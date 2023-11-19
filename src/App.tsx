359538ce24e702c96813b32ef250e3e800b64838
Giancarlo Vera <giancarlo.vera@globant.com>
2023-11-19 01:06:08 -0500

    setup frontend with react using vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>Giancarlo Vera</h1>
+    </>
+  );
+}
+
+export default App;
