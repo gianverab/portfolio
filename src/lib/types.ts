dfa40e63f393eb27141c861f2fcf574796ccdb34
Giancarlo Vera <giancarlo.vera@globant.com>
2023-11-19 23:37:25 -0500

    add navbar component

---
+++ src/lib/types.ts
@@ -0,0 +1,10 @@
+// Define TypeScript enum for menu items
+export enum MenuItems {
+  Home = "HOME",
+  About = "ABOUT",
+  Work = "WORK",
+  Skills = "SKILLS",
+  Experience = "EXPERIENCE",
+  Testimonials = "TESTIMONIALS",
+  Contact = "CONTACT",
+}
